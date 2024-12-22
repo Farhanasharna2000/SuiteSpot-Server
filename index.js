c648b7e842edc58016d29d8220089b37c0fd2ed3
Farhanasharna2000 <farhanasharna22@gmail.com>
2024-12-22 23:44:21 +0600

    delete a booking from db

--- index.js
+++ index.js
@@ -140,7 +140,13 @@ async function run() {
   res.send(result)
 })
     
-  
+    //delete a booking from db
+    app.delete('/booking/:id',verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingsCollection.deleteOne(query)
+      res.send(result)
+    })
   
     
     
